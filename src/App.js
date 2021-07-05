8fb6b47743a08db18b45128cc8c3297bf87753b5
Harshit <bkharshitbk@gmail.com>
2021-07-05 17:45:11 +0530

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
-
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
 function App() {
-    return <div>HI</div>;
+    return (
+        <Switch>
+            <Route exact path="/">
+                This is homepage
+            </Route>
+            <Route exact path="/ab">
+                Error
+            </Route>
+
+            {/* page not found */}
+            <Route>
+                <h4>This Page is not Present</h4>
+                <h1>404 Error</h1>
+            </Route>
+        </Switch>
+    );
 }
 
 export default App;
