888ba9832b5d11beb48a9ad6f81076cb5561982b
Harshit <bkharshitbk@gmail.com>
2021-07-09 01:43:14 +0530

    add search and fetch result from MAZETV api

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MainPageLayout } from '../myComponents/MainPageLayout';
 
 export const HomePage = () => {
-    return <MainPageLayout>Welcome to HomePage</MainPageLayout>;
+    const [input, setInput] = useState('');
+
+    const onInputChange = ev => {
+        setInput(ev.target.value);
+    };
+
+    const onSearch = () => {
+        // https://api.tvmaze.com/search/shows?q=aliens
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+            .then(r => r.json())
+            .then(result => {
+                console.log(result);
+            });
+    };
+
+    const onKeyDown = ev => {
+        if (ev.keyCode === 13) {
+            onSearch();
+        }
+    };
+
+    return (
+        <MainPageLayout>
+            <input
+                type="text"
+                onChange={onInputChange}
+                onKeyDown={onKeyDown}
+                value={input}
+            />
+            <button type="button" onClick={onSearch}>
+                Search
+            </button>
+        </MainPageLayout>
+    );
 };
