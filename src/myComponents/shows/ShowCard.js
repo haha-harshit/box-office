c42ccd21957435d0782e88398c67322123e547a5
Harshit <bkharshitbk@gmail.com>
2021-07-17 17:39:11 +0530

    defined ShowCard

--- src/myComponents/shows/ShowCard.js
+++ src/myComponents/shows/ShowCard.js
@@ -1,4 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
-export const ShowCard = ({ id, image, name, summary }) => {};
+export const ShowCard = ({ id, image, name, summary }) => {
+    const summaryAsText = summary
+        ? `${summary
+              .split(' ')
+              .slice(0, 10)
+              .join(' ')
+              .replace(/<.+?>/g, '')}...`
+        : 'No Description';
+
+    return (
+        <div>
+            <div>
+                <img src={image} alt="show-image" />
+            </div>
+
+            <h1>{name}</h1>
+            <p>{summaryAsText}</p>
+            <div>
+                <Link to={`/show/${id}`}>Read More</Link>
+                <button type="button">Star Me</button>
+            </div>
+        </div>
+    );
+};
