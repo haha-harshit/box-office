109441be1f87b5a60f09d04ef26370d9c46bfb34
Harshit <bkharshitbk@gmail.com>
2021-07-17 17:52:18 +0530

    add ActorGrid

--- src/myComponents/actors/ActorGrid.js
+++ src/myComponents/actors/ActorGrid.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { ActorCard } from './ActorCard';
 
-export const ActorGrid = () => {
-    return <div></div>;
+export const ActorGrid = ({ data }) => {
+    return (
+        <div>
+            {data.map(({ person }) => (
+                <ActorCard
+                    key={person.id}
+                    name={person.name}
+                    country={person.country ? person.country.name : null}
+                    birthday={person.birthday}
+                    deathday={person.deathday}
+                    gender={person.gender}
+                    image={
+                        person.image ? person.image.medium : `Image Not Found`
+                    }
+                />
+            ))}
+        </div>
+    );
 };
