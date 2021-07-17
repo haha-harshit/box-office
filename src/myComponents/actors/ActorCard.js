e7ad262c2988d2b63926d85760daebfc170a007f
Harshit <bkharshitbk@gmail.com>
2021-07-17 18:09:20 +0530

    add ActorCard

--- src/myComponents/actors/ActorCard.js
+++ src/myComponents/actors/ActorCard.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
-export const ActorCard = () => {
-    return <div></div>;
+export const ActorCard = ({
+    image,
+    name,
+    gender,
+    country,
+    birthday,
+    deathday,
+}) => {
+    return (
+        <div>
+            <div>
+                <img src={image} alt="actorImage" />
+            </div>
+            <h1>
+                {name} {gender ? `(${gender})` : null}
+            </h1>
+            <p>{country ? `Comes from ${country}` : 'No Country Known'}</p>
+            {birthday ? <p>Born {birthday}</p> : null}
+            <p>{deathday ? `Died ${deathday}` : 'Alive'}</p>
+        </div>
+    );
 };
