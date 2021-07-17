401167a4dcbb7164f1eebc035fb3a361d2d3cbfe
Harshit <bkharshitbk@gmail.com>
2021-07-17 17:36:07 +0530

    add ShowCard comp. in ShowGrid

--- src/myComponents/shows/ShowGrid.js
+++ src/myComponents/shows/ShowGrid.js
@@ -2,5 +2,17 @@ import React from 'react';
 import { ShowCard } from './ShowCard';
 
 export const ShowGrid = ({ data }) => {
-    return <div></div>;
+    return (
+        <div>
+            {data.map(({ show }) => (
+                <ShowCard
+                    key={show.id}
+                    id={show.id}
+                    name={show.name}
+                    image={show.image ? show.image.medium : `Image Not Found`}
+                    summary={show.summary}
+                />
+            ))}
+        </div>
+    );
 };
