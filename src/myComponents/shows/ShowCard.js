d080847361818f2b1f8341f36f7a061c02fcf2ee
Harshit <bkharshitbk@gmail.com>
2021-07-18 18:13:14 +0530

    add searchCard in ShowCard

--- src/myComponents/shows/ShowCard.js
+++ src/myComponents/shows/ShowCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { SearchCard } from '../styled';
 
 export const ShowCard = ({ id, image, name, summary }) => {
     const summaryAsText = summary
@@ -11,7 +12,7 @@ export const ShowCard = ({ id, image, name, summary }) => {
         : 'No Description';
 
     return (
-        <div>
+        <SearchCard>
             <div>
                 <img src={image} alt="show-image" />
             </div>
@@ -22,6 +23,6 @@ export const ShowCard = ({ id, image, name, summary }) => {
                 <Link to={`/show/${id}`}>Read More</Link>
                 <button type="button">Star Me</button>
             </div>
-        </div>
+        </SearchCard>
     );
 };
