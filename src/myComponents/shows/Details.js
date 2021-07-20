fe818d4008e1356fbd48e747b3d6769b7db8af76
Harshit <bkharshitbk@gmail.com>
2021-07-20 21:17:50 +0530

    add details comp.

--- src/myComponents/shows/Details.js
+++ src/myComponents/shows/Details.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
-export const Details = () => {
-    return <div></div>;
+export const Details = ({ status, premiered, network }) => {
+    return (
+        <div>
+            <p>
+                Status: <span>{status}</span>
+            </p>
+            <p>
+                Premiered {premiered} {network ? `on ${network.name}` : null}
+            </p>
+        </div>
+    );
 };
+
+export default Details;
