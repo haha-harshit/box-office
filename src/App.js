58a4039d8b04fe248f297104a9c7e939225ba34b
Harshit <bkharshitbk@gmail.com>
2021-07-22 21:01:51 +0530

    defined theme for components

--- src/App.js
+++ src/App.js
@@ -8,26 +8,38 @@ import { Starred } from './Pages/Starred';
 import { NotFound } from './Pages/NotFound';
 import { Show } from './Pages/Show';
 
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+    mainColors: {
+        blue: '#2400ff',
+        gray: '#c6c6c6',
+        dark: '#353535',
+    },
+};
+
 function App() {
     return (
-        <Switch>
-            <Route exact path="/">
-                <HomePage />
-            </Route>
+        <ThemeProvider theme={theme}>
+            <Switch>
+                <Route exact path="/">
+                    <HomePage />
+                </Route>
 
-            <Route exact path="/starred">
-                <Starred />
-            </Route>
+                <Route exact path="/starred">
+                    <Starred />
+                </Route>
 
-            <Route exact path="/show/:id">
-                <Show />
-            </Route>
+                <Route exact path="/show/:id">
+                    <Show />
+                </Route>
 
-            {/* page not found */}
-            <Route>
-                <NotFound />
-            </Route>
-        </Switch>
+                {/* page not found */}
+                <Route>
+                    <NotFound />
+                </Route>
+            </Switch>
+        </ThemeProvider>
     );
 }
 
