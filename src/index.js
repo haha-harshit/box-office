1b41c0178aededb1d39af1ea26a9d979ef4ae3fe
Harshit <bkharshitbk@gmail.com>
2021-07-24 18:37:42 +0530

    add service workers for PWA

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWokerRegistration';
 
 ReactDOM.render(
     <HashRouter>
@@ -16,3 +17,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
